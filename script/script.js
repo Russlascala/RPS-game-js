7a463adab1d2f8b937458d364dc7191d35de2891
Russ LaScala <32844708+Russlascala@users.noreply.github.com>
2018-10-09 09:25:41 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,13 +3,13 @@ function playGame(btn_id) {
   var playerImg = document.getElementById("playerImg");
 
   if (btn_id === "rock"){
-    playerImg.src = "../imgs/rockP.png";
+    playerImg.src = "./imgs/rockP.png";
     console.log('rock');
   } else if (btn_id === "paper"){
-    playerImg.src = "../imgs/paperP.png";
+    playerImg.src = "./imgs/paperP.png";
     console.log('paper');
   } else if (btn_id === "scissors"){
-    playerImg.src = "../imgs/scissorsP.png";
+    playerImg.src = "./imgs/scissorsP.png";
     console.log("scissors");
   } else{
     console.log("error in the players choice");
@@ -21,9 +21,9 @@ function playGame(btn_id) {
   compPic = Math.floor(Math.random() * 3 + 1);
   console.log(compPic);
   if (compPic === 1){
-    compImg.src = "../imgs/rockc.png";
+    compImg.src = "./imgs/rockc.png";
   } else if (compPic === 2){
-    compImg.src = "../imgs/paperC.png"
+    compImg.src = "./imgs/paperC.png"
   } else if (compPic === 3) {
     compImg.src = compImgs[2];
   } else{
